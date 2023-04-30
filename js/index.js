984ff5d88373e36a71e54b32729a6bcb3731bdb1
AlenaVP <poprugo@gmail.com>
2023-04-30 22:44:57 +0300

    feat: add style

--- js/index.js
+++ js/index.js
@@ -1,5 +1,14 @@
-import Utils from './Utils.js';
+import Label from './Label.js';
+import Textarea from './Textarea.js';
+
+import Keyboard from './Keyboard.js';
+
+import Paragraph from './Paragraph.js';
 
 const body = document.querySelector('body');
 
-body.append(Utils.prototype.htmlToElement('<p>Hello world!</p>)'));
+body.append(new Label().element);
+body.append(new Textarea().element);
+body.append(new Keyboard().element);
+body.append(new Paragraph('description', 'Клавиатура создана в операционной системе Windows').element);
+body.append(new Paragraph('language', 'Для переключения языка комбинация: левыe ctrl + alt').element);
