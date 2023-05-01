eefccaeb7d80ae287614ea9f2077faa265bad0a7
AlenaVP <poprugo@gmail.com>
2023-05-01 19:35:48 +0300

    feat: add toggleCapsLock()

--- js/index.js
+++ js/index.js
@@ -8,7 +8,62 @@ const appContainer = new AppContainer();
 
 body.prepend(appContainer.container);
 
-const renderChar = function renderChar(event) {
+function getVisibleKeyChilds() {
+  const visibleKeyChilds = [];
+  const keys = document.querySelectorAll('.key');
+  keys.forEach((key) => {
+    const keyChildren = [...key.children];
+    keyChildren.forEach((child) => {
+      if (child.tagName === 'SPAN' && !child.classList.contains('hidden')) {
+        visibleKeyChilds.push(child);
+      }
+    });
+  });
+  return visibleKeyChilds;
+}
+
+function useCaps() {
+  const visibleKeyChilds = getVisibleKeyChilds();
+  visibleKeyChilds.forEach((child) => {
+    const spans = [...child.children];
+    spans.forEach((span) => {
+      if (span.tagName === 'SPAN') {
+        if (span.classList.contains('caps')) {
+          span.classList.remove('hidden');
+        } else {
+          span.classList.add('hidden');
+        }
+      }
+    });
+  });
+}
+
+function useLowerCase() {
+  const visibleKeyChilds = getVisibleKeyChilds();
+  visibleKeyChilds.forEach((child) => {
+    const spans = [...child.children];
+    spans.forEach((span) => {
+      if (span.tagName === 'SPAN') {
+        if (span.classList.contains('caseDown')) {
+          span.classList.remove('hidden');
+        } else {
+          span.classList.add('hidden');
+        }
+      }
+    });
+  });
+}
+
+function toggleCapsLock(key) {
+  key.classList.toggle('active');
+  if (key.classList.contains('active')) {
+    useCaps();
+  } else {
+    useLowerCase();
+  }
+}
+
+function renderChars(event) {
   let key;
   let textContent;
   if (event.type === 'mousedown') {
@@ -21,7 +76,7 @@ const renderChar = function renderChar(event) {
     key = document.querySelector(`.${event.code}`);
   }
   if (key.classList.contains('CapsLock')) {
-    key.classList.toggle('active');
+    toggleCapsLock(key);
   } else {
     key.classList.add('active');
     const isActions = actions.controls.some((c) => key.classList.contains(c));
@@ -29,17 +84,17 @@ const renderChar = function renderChar(event) {
     const { textarea } = appContainer;
     textarea.textContent = Utils.prototype.addText(isActions, isArrows, textarea, textContent);
   }
-};
+}
 
-const deaktivateKey = function deaktivateKey() {
+function deaktivateKey() {
   const keys = [...document.querySelectorAll('.key')];
   keys.filter((k) => k.classList.contains('active') && !k.classList.contains('CapsLock')).forEach((k) => k.classList.remove('active'));
-};
+}
 
-appContainer.keyboard.addEventListener('mousedown', renderChar);
+appContainer.keyboard.addEventListener('mousedown', renderChars);
 
 appContainer.keyboard.addEventListener('mouseup', deaktivateKey);
 
-document.addEventListener('keydown', renderChar);
+document.addEventListener('keydown', renderChars);
 
 document.addEventListener('keyup', deaktivateKey);
