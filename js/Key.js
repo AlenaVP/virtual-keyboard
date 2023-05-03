679f5513967e675fb1d0d883be1889b804f769c3
AlenaVP <poprugo@gmail.com>
2023-05-03 17:12:24 +0300

    refactor: change a key creation

--- js/Key.js
+++ js/Key.js
@@ -1,20 +1,20 @@
 import AppElement from './AppElement.js';
+import Utils from './Utils.js';
 
 export default class Key extends AppElement {
   constructor(keyObj) {
-    super(`<div class="key ${keyObj.keyName}">
-  <span class="rus hidden">
-    <span class="caseDown hidden">${keyObj.ru.down}</span>
-    <span class="caseUp hidden">${keyObj.ru.up}</span>
-    <span class="caps hidden">${keyObj.ru.caps || keyObj.ru.up}</span>
-    <span class="shiftCaps hidden">${keyObj.ru.shiftCaps || keyObj.ru.down}</span>
-  </span>
-  <span class="eng">
-    <span class="caseDown">${keyObj.en.down}</span>
-    <span class="caseUp hidden">${keyObj.en.up}</span>
-    <span class="caps hidden">${keyObj.en.caps || keyObj.en.up}</span>
-    <span class="shiftCaps hidden">${keyObj.en.shiftCaps || keyObj.en.down}</span>
-  </span>
-</div>`);
+    super(`<div class="key ${keyObj.keyName}"></div>`);
+    let langSpan = Utils.prototype.htmlToElement('<span class="rus hidden"></span>');
+    langSpan.appendChild(Utils.prototype.htmlToElement(`<span class="caseDown hidden">${keyObj.ru.down}</span>`));
+    langSpan.appendChild(Utils.prototype.htmlToElement(`<span class="caseUp hidden">${keyObj.ru.up}</span>`));
+    langSpan.appendChild(Utils.prototype.htmlToElement(`<span class="caps hidden">${keyObj.ru.caps || keyObj.ru.up}</span>`));
+    langSpan.appendChild(Utils.prototype.htmlToElement(`<span class="shiftCaps hidden">${keyObj.ru.shiftCaps || keyObj.ru.down}</span>`));
+    this.element.append(langSpan);
+    langSpan = Utils.prototype.htmlToElement('<span class="eng"></span>');
+    langSpan.appendChild(Utils.prototype.htmlToElement(`<span class="caseDown">${keyObj.en.down}</span>`));
+    langSpan.appendChild(Utils.prototype.htmlToElement(`<span class="caseUp hidden">${keyObj.en.up}</span>`));
+    langSpan.appendChild(Utils.prototype.htmlToElement(`<span class="caps hidden">${keyObj.en.caps || keyObj.en.up}</span>`));
+    langSpan.appendChild(Utils.prototype.htmlToElement(`<span class="shiftCaps hidden">${keyObj.en.shiftCaps || keyObj.en.down}</span>`));
+    this.element.append(langSpan);
   }
 }
